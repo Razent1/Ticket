2ab73bb3275f8906a50752392e69c746361a3daf
Razent1 <53121795+Razent1@users.noreply.github.com>
2020-03-01 20:04:27 +0200

    RegistrationForm // checkBx // btn

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 import './Login/style.css'
 import './Header/styleHeader.css'
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
+
 import Header from "./Header/Header";
 import Login from "./Login/Login";
+import RegistrationForm from "./RegistrationForm/RegistrationForm";
 
 
 class App extends React.Component {
@@ -12,9 +14,9 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Header/>
-                <Login/>
-                {/*<RegistrationForm/>*/}
+                {/*<Header/>*/}
+                {/*<Login/>*/}
+                <RegistrationForm/>
             </div>
         )
 
--- src/RegistrationForm/RegistrationForm.js
+++ src/RegistrationForm/RegistrationForm.js
@@ -5,47 +5,50 @@ class RegistrationForm extends React.Component {
 
     render() {
         return (
-            <div className='container w-75'>
-                <div className='col'>Registration</div>
+            <div className='container w-100'>
+                <div className='regist flex-column'><u>REGISTRATION</u></div>
                 <div className='d-flex justify-content-center'>
                     <div className='flex-column w-50'>
                         <div className='title d-inline-flex p-4'>
-                            Salutation and address
+                            Contact information
                         </div>
                         <div className='salutations text-left'>
-                            <div className='flex-column dropdown w-100'>
-                                <button className=" drp1 btn w-100 btn-secondary dropdown-toggle text-left"
-                                        type="button"
-                                        id="dropdownMenu2"
-                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Mr
-                                </button>
-                                <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
-                                    <button className="dropdown-item" type="button">Mrs/Miss</button>
-                                </div>
-
+                            <div className='flex-column form-inline w-100'>
+                                <select className="drp1 w-100 custom-select my-1 mr-sm-2"
+                                        id="inlineFormCustomSelectPref">
+                                    <option selected>Mr.</option>
+                                    <option value="1">Mrs/Miss</option>
+                                </select>
                             </div>
-                            <div className='col'><input className='w-100' type="text" placeholder='Name'/></div>
-                            <div className='col'><input className='w-100' type="text" placeholder='Surname'/></div>
-                            <div className='col'><input className='w-100' type="text" placeholder='Company'/></div>
-                            <div className='col'><input className='w-100' type="text" placeholder='Street'/></div>
-                            <div className='col'><input className='w-100' type="text" placeholder='House'/></div>
-                            <div className='col'><input className='w-100' type="text" placeholder='Additional info'/></div>
-                            <div className='col'><input className='w-100' type="text" placeholder='Postcode'/></div>
-                            <div className='col'><input className='w-100' type="text" placeholder='City'/></div>
-                            <div className='col'><input className='w-100' type="text" placeholder='Country'/></div>
+                            <div className='flex-column'><input className='w-100' type="text" placeholder='Name'/></div>
+                            <div className='flex-column'><input className='w-100' type="text" placeholder='Surname'/></div>
+                            <div className='flex-column'><input className='w-100' type="text" placeholder='Email'/></div>
+                            <div className='flex-column'><input className='w-100' type="text" placeholder='Password'/></div>
+                            <div className='flex-column'><input className='w-100' type="text" placeholder='Confirm Password'/></div>
+                            <div className='flex-column'><input className='w-100' type="text" placeholder='Phone Number'/></div>
+
                         </div>
-                    </div>
-                    <div className='flex-column'>
-                        <div className='title d-inline-flex p-4'>
-                            Contact Information
+                        <div className='check flex-column text-left small'>
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                                <label className="form-check-label" htmlFor="defaultCheck1">
+                                    I have read
+                                </label>
+                            </div>
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox" value="" id="defaultCheck2"/>
+                                <label className="form-check-label" htmlFor="defaultCheck2">
+                                    I agree
+                                </label>
+                            </div>
                         </div>
-                        <div className='col'>Email</div>
-                        <div className='col'>Password</div>
-                        <div className='col'>Confirm Password</div>
-                        <div className='col'>Phone Number</div>
-                        <div className='col'>Additional phone number</div>
+
+                        <div className='flex-column'>
+                            <button className='btnRegister'>Register</button>
+                        </div>
+
                     </div>
+
                 </div>
             </div>
         )
