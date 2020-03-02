d6e1ae6b6b7665362ef156c86ae02a614253cb8b
Razent1 <53121795+Razent1@users.noreply.github.com>
2020-03-02 22:22:55 +0200

    RegistrationForm // preparations to API

--- src/RegistrationForm/RegistrationForm.js
+++ src/RegistrationForm/RegistrationForm.js
@@ -3,7 +3,35 @@ import './registrationStyle.css'
 
 class RegistrationForm extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            gender: '1',
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            phoneNumber: ''
+        }
+        this.onChangeInput = this.onChangeInput.bind(this);
+    }
+
+    handleClick = (event) => {
+        this.setState({});
+    }
+
+    onChangeInput = (event) => {
+        const name = event.target.name;
+        this.setState({[name]: event.target.value});
+    }
+
+    onClickSelect = (event) => {
+        this.setState({gender: event.target.value});
+    }
+
+
     render() {
+        console.log(this.state.gender);
         return (
             <div className='container w-100'>
                 <div className='regist flex-column'><u>REGISTRATION</u></div>
@@ -14,18 +42,37 @@ class RegistrationForm extends React.Component {
                         </div>
                         <div className='salutations text-left'>
                             <div className='flex-column form-inline w-100'>
-                                <select className="drp1 w-100 custom-select my-1 mr-sm-2"
+                                <select onChange={this.onClickSelect} className="drp1 w-100 custom-select my-1 mr-sm-2"
                                         id="inlineFormCustomSelectPref">
-                                    <option selected>Mr.</option>
-                                    <option value="1">Mrs/Miss</option>
+                                    <option value='1'>Mr.</option>
+                                    <option value='2'>Mrs/Miss</option>
                                 </select>
                             </div>
-                            <div className='flex-column'><input className='w-100' type="text" placeholder='Name'/></div>
-                            <div className='flex-column'><input className='w-100' type="text" placeholder='Surname'/></div>
-                            <div className='flex-column'><input className='w-100' type="text" placeholder='Email'/></div>
-                            <div className='flex-column'><input className='w-100' type="text" placeholder='Password'/></div>
-                            <div className='flex-column'><input className='w-100' type="text" placeholder='Confirm Password'/></div>
-                            <div className='flex-column'><input className='w-100' type="text" placeholder='Phone Number'/></div>
+                            <div className='flex-column'><input name="firstName" className='w-100' type="text"
+                                                                placeholder='Name'
+                                                                value={this.state.firstName}
+                                                                onChange={this.onChangeInput}/></div>
+                            <div className='flex-column'><input name="lastName" className='w-100' type="text"
+                                                                placeholder='Surname'
+                                                                value={this.state.lastName}
+                                                                onChange={this.onChangeInput}/>
+                            </div>
+                            <div className='flex-column'><input name="email" className='w-100' type="email"
+                                                                placeholder='Email'
+                                                                value={this.state.email} onChange={this.onChangeInput}/>
+                            </div>
+                            <div className='flex-column'><input name="password" className='w-100' type="text"
+                                                                placeholder='Password'
+                                                                value={this.state.password}
+                                                                onChange={this.onChangeInput}/>
+                            </div>
+                            <div className='flex-column'><input className='w-100' type="text"
+                                                                placeholder='Confirm Password'
+                            /></div>
+                            <div className='flex-column'><input name='phoneNumber' className='w-100' type="text"
+                                                                placeholder='Phone Number'
+                                                                value={this.state.phoneNumber}
+                                                                onChange={this.onChangeInput}/></div>
 
                         </div>
                         <div className='check flex-column text-left small'>
@@ -44,11 +91,9 @@ class RegistrationForm extends React.Component {
                         </div>
 
                         <div className='flex-column'>
-                            <button className='btnRegister'>Register</button>
+                            <button onClick={this.handleClick} className='btnRegister'>Register</button>
                         </div>
-
                     </div>
-
                 </div>
             </div>
         )
