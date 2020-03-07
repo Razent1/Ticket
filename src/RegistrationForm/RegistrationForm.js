ff325a0137882dec438dc3157a15b6084340595e
Razent1 <53121795+Razent1@users.noreply.github.com>
2020-03-07 23:15:19 +0200

    RegistrationForm // API // final version of RegistrationForm

--- src/RegistrationForm/RegistrationForm.js
+++ src/RegistrationForm/RegistrationForm.js
@@ -18,15 +18,21 @@ class RegistrationForm extends React.Component {
             phoneNumber: '',
             firstRender: false,
             fieldsErrors: {},
-            err: false // if we catch errors, we will render our page with new error fields
+            err: false, // if we catch errors, we will render our page with new error fields
+            errFirstName: '',
+            errLastName: '',
+            errEmail: '',
+            errPassword: '',
+            errConfirmPassword: '',
+            errPhoneNumber: ''
         }
         this.onChangeInput = this.onChangeInput.bind(this);
         this.handleClick = this.handleClick.bind(this);
+        this.errorsOnScreen = this.errorsOnScreen.bind(this);
     }
 
     handleClick = async () => {
         let parseObj;
-        this.setState({firstRender: true});
         const data = {
             "gender": parseInt(this.state.gender),
             "firstName": this.state.firstName,
@@ -63,12 +69,28 @@ class RegistrationForm extends React.Component {
                 console.error(error);
             }
         } else {
-            console.log("Incorrect password");
+            this.setState({err: true});
+            this.setState({errFirstName: 'Name'});
+            this.setState({errLastName: 'Surname'});
+            this.setState({errEmail: 'Email'});
+            this.setState({errPassword: "Passwords don't match"});
+            this.setState({errPhoneNumber: 'Phone Number'});
+            this.setState({errConfirmPassword: "Passwords don't match"});
         }
 
-        this.setState({fieldsErrors: parseObj});
-        console.log(this.state.fieldsErrors.fields.lastName[0]);
+        this.setState({firstName: ''}); //обнуляем значения после кнопки регистрации, чтобы было видно плейсхолдеры с ошибкой
+        this.setState({lastName: ''});
+        this.setState({email: ''});
+        this.setState({password: ''});
+        this.setState({confirmPassword: ''});
+        this.setState({phoneNumber: ''});
+        this.setState({errConfirmPassword: 'Confirm Password'});
 
+        if (parseObj != null) {
+            await this.setState({fieldsErrors: parseObj});
+            await this.errorsOnScreen();
+        }
+        await this.setState({firstRender: true});
     }
 
     onChangeInput = (event) => {
@@ -80,9 +102,36 @@ class RegistrationForm extends React.Component {
         this.setState({gender: event.target.value});
     }
 
+    errorsOnScreen = () => {
+        if (this.state.fieldsErrors.fields.firstName) {
+            this.setState({errFirstName: this.state.fieldsErrors.fields.firstName[0]});
+        } else {
+            this.setState({errFirstName: "Name"});
+        }
+        if (this.state.fieldsErrors.fields.lastName) {
+            this.setState({errLastName: this.state.fieldsErrors.fields.lastName[0]});
+        } else {
+            this.setState({errLastName: "Surname"});
+        }
+        if (this.state.fieldsErrors.fields.email) {
+            this.setState({errEmail: this.state.fieldsErrors.fields.email[0]});
+        } else {
+            this.setState({errEmail: "Email"});
+        }
+        if (this.state.fieldsErrors.fields.password) {
+            this.setState({errPassword: this.state.fieldsErrors.fields.password[0]});
+        } else {
+            this.setState({errPassword: "Email"});
+        }
+        if (this.state.fieldsErrors.fields.phoneNumber) {
+            this.setState({errPhoneNumber: this.state.fieldsErrors.fields.phoneNumber[0]});
+        } else {
+            this.setState({errPhoneNumber: "Email"});
+        }
+    }
+
 
     render() {
-        console.log(this.state.gender);
         if (this.state.firstRender === false) {
             return (
                 <div className='container w-100'>
@@ -163,7 +212,78 @@ class RegistrationForm extends React.Component {
             }
             if (this.state.err === true) { //рендер в случае наличия ошибок при вводе данных
                 return (
-                    <div>TODO</div>
+                    <div className='container w-100'>
+                        <div className='regist flex-column'><u>REGISTRATION</u></div>
+                        <div className='d-flex justify-content-center'>
+                            <div className='flex-column w-50'>
+                                <div className='title d-inline-flex p-4'>
+                                    Contact information
+                                </div>
+                                <div className='salutations text-left'>
+                                    <div className='flex-column form-inline w-100'>
+                                        <select onChange={this.onClickSelect}
+                                                className="drp1 w-100 custom-select my-1 mr-sm-2"
+                                                id="inlineFormCustomSelectPref">
+                                            <option value='1'>Mr.</option>
+                                            <option value='2'>Mrs/Miss</option>
+                                        </select>
+                                    </div>
+                                    <div className='flex-column'><input name="firstName" className='w-100' type="text"
+
+                                                                        placeholder={this.state.errFirstName}
+                                                                        value={this.state.firstName}
+                                                                        onChange={this.onChangeInput}/></div>
+                                    <div className='flex-column'><input name="lastName" className='w-100' type="text"
+                                                                        placeholder={this.state.errLastName}
+                                                                        value={this.state.lastName}
+                                                                        onChange={this.onChangeInput}/>
+                                    </div>
+                                    <div className='flex-column'><input name="email" className='w-100' type="email"
+                                                                        placeholder={this.state.errEmail}
+                                                                        value={this.state.email}
+                                                                        onChange={this.onChangeInput}/>
+                                    </div>
+                                    <div className='flex-column'><input name="password" className='w-100'
+                                                                        type="password"
+                                                                        placeholder={this.state.errPassword}
+                                                                        value={this.state.password}
+                                                                        onChange={this.onChangeInput}/>
+                                    </div>
+                                    <div className='flex-column'><input name="confirmPassword" className='w-100'
+                                                                        type="password"
+                                                                        placeholder={this.state.errConfirmPassword}
+                                                                        value={this.state.confirmPassword}
+                                                                        onChange={this.onChangeInput}
+                                    /></div>
+                                    <div className='flex-column'><input name='phoneNumber' className='w-100' type="text"
+                                                                        placeholder={this.state.errPhoneNumber}
+                                                                        value={this.state.phoneNumber}
+                                                                        onChange={this.onChangeInput}/></div>
+
+                                </div>
+                                <div className='check flex-column text-left small'>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="checkbox" value=""
+                                               id="defaultCheck1"/>
+                                        <label className="form-check-label" htmlFor="defaultCheck1">
+                                            I have read
+                                        </label>
+                                    </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="checkbox" value=""
+                                               id="defaultCheck2"/>
+                                        <label className="form-check-label" htmlFor="defaultCheck2">
+                                            I agree
+                                        </label>
+                                    </div>
+                                </div>
+
+                                <div className='flex-column'>
+                                    <button onClick={this.handleClick} className='btnRegister'>Register</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 )
             }
         }
